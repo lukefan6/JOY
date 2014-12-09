e548edab3f28bde64dc3ffad7cee0c5f88f0302f
lukefan6 <wakefan6@gmail.com>
2014-12-09 18:27:51 +0800

    Retreive MRT station info from trtc.com.tw

---
+++ Joy.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Joy.Core;
+namespace Joy.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allStations = MrtManager.GetAllStations();
+            var random = new Random();
+            var nextLine = allStations.ToArray()[random.Next(allStations.Count())];
+            System.Console.WriteLine(nextLine.Name);
+            var nextStation = nextLine.Stations.ToArray()[random.Next(nextLine.Stations.Count())];
+            System.Console.WriteLine(nextStation.Name);
+            System.Console.ReadKey();
+        }
+    }
+}
